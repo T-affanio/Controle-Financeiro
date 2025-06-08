a7fc29aefefbe9d7faa7c28114834302acbe6ed1
affanio <thiagoaff@gmail.com>
2025-06-08 20:08:13 -0300

    Controle financeiro

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <div className="bg-blue-800 h-40 text-center">
-                <h1 className="pt-7 text-3xl font-semibold">Finance System</h1>
+                <h1 className="pt-7 text-3xl font-semibold">Controle Financeiro</h1>
         </div>
     )
 }
